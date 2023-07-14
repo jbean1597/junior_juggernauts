37a2f81ffe2811cb81ed927f07da1b107577a6a5
americanchuy <americanchuy10@gmail.com>
2023-07-13 20:36:56 -0700

    adding basic app with openai and .gitignore

--- app_folder/app.py
+++ app_folder/app.py
@@ -1,13 +1,19 @@
+from dotenv import load_dotenv, find_dotenv
 import streamlit as st
+from langchain.llms import OpenAI
+import os
+import openai
+load_dotenv(find_dotenv())
+openai.api_key = os.environ['OPENAI_API_KEY']
+API_KEY = os.environ['OPENAI_API_KEY']
 
-
-
-
+llm = OpenAI(temperature = .9)
 st.title('Welcome to the Computer Science Learning tool.')
 
 
-question = st.text_input('Please enter your question here: ')
 
+question = st.text_input('Please enter your question here: ')
+answer = llm(question)
 
 if st.button('Submit'):
     if question == "":
@@ -15,4 +21,4 @@ if st.button('Submit'):
     else:
         output = f"The answer to {question} is blah blah blah"
 
-    st.write(output)
+    st.write(answer)
